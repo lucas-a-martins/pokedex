85c6aa639438436a4e040447142a7e84005e6684
Lucas Martins <l.antonio@grad.ufsc.br>
2023-08-24 02:36:26 -0300

    adicionado imagens e flavor text

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,5 +1,5 @@
 function fetchPokedexApi() {
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=1010').then((response) => response.json())
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=1008').then((response) => response.json())
     .then((data) => {
         let pokedex = document.querySelector('#pokedex');
 
@@ -15,10 +15,9 @@ function fetchPokedexApi() {
             pokemon.appendChild(pokeButton);
 
             fetch(data.results[i].url).then((response) => response.json()).then((description) => {
-                //console.log(description)
 
                 let pokeDescription = document.createElement('div');
-                pokeDescription.classList.add('description', 'hidden');
+                pokeDescription.classList.add('hidden', 'description');
                 pokeDescription.setAttribute('id', pokemonName);
                 pokemon.appendChild(pokeDescription);
                 pokeButton.onclick = () => {
@@ -26,26 +25,50 @@ function fetchPokedexApi() {
                 };
 
                 let pokeInfo = document.createElement('p');
-                let text = [];
-                pokeInfo.innerText = 'National nº: #' + pokemon.id.toString().padStart(4, '0');
+                pokeInfo.innerText = 'National nº: #' + description.id.toString().padStart(4, '0');
                 pokeDescription.appendChild(pokeInfo);
 
                 let allTypes = document.createElement('div');
+                allTypes.classList.add('type-div');
                 allTypes.appendChild(addType(description.types[0].type.name));
                 if (description.types.length == 2) {
                     allTypes.appendChild(addType(description.types[1].type.name));
                 }
                 pokeDescription.appendChild(allTypes);
+
+                let pokeImg = document.createElement('div');
+                pokeImg.appendChild(addImg(description.sprites.other['official-artwork'].front_default));
+                pokeDescription.appendChild(pokeImg);
+
+                fetch(description.species.url).then((response) => response.json()).then((flavorText) => {
+                    let flavorTextEntries = document.createElement('div');
+                    flavorTextEntries.classList.add('poke-info');
+
+
+                    let cleanedFlavorText = flavorText.flavor_text_entries[0].flavor_text.replace(/\n/g, ' ');
+                    cleanedFlavorText = cleanedFlavorText.replace(/[^a-zA-Z0-9.,!?áéíóúÁÉÍÓÚâêîôûÂÊÎÔÛàèìòùÀÈÌÒÙçÇãõÃÕ ]/g, '');
+
+
+                    flavorTextEntries.innerText = cleanedFlavorText;
+                    pokeDescription.appendChild(flavorTextEntries);
+
+                });
             })
         }
     })
+}
+
+function addImg(src) {
+    let pokeImg = document.createElement('img');
+    pokeImg.src = src;
+    pokeImg.classList.add('main-img');
+    return pokeImg;
 }
 
 function addType(type) {
     let buttonType = document.createElement('button');
     buttonType.classList.add(type, 'type-icon');
     buttonType.innerText = type;
-
     return buttonType;
 }
 
